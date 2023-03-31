68393c6bc3ac69d96c20dbaa3e9a6bcb1c79eaad
Mikee9165 <tconstantin906@gmail.com>
2023-03-31 18:02:37 +0300

    Laborator 5 - exercitii matrice

--- Laborator 5/main.py
+++ Laborator 5/main.py
@@ -9,13 +9,16 @@ def generare_vector(a, b, n):
     a = int(a)
     b = int(b)
     while a>=b:
-        a, b = input("Valorea minima trebuie sa fie mai mica decat valoarea maxima!\nReintrodu valorile intervaluli (a, b]:").split()
+        a, b = input("Valorea minima trebuie sa fie mai mica decat valoarea maxima!\nReintrodu valorile intervalului (a, b]:").split()
         a = int(a)
         b = int(b)
     vector = []
     for i in range(n):
         vector.append(randint(a+1, b))
     return vector
+
+def afisare_vector(vector):
+    print("Vectorul este: ", *vector)
 
 def media_aritmetica(vector):
     ma = sum(vector)/len(vector)
@@ -27,9 +30,9 @@ def media_aritmetica(vector):
 def valoare_maxima(vector):
     maxx = vector[0]
     pozitie = 0
-    for i in vector:
-        if i>maxx:
-            maxx = i
+    for i in range(len(vector)):
+        if vector[i]>maxx:
+            maxx = vector[i]
             pozitie = i
     print(f"Valoarea maxima din vector este [{maxx}], pe pozitia [{pozitie}]")
 
@@ -44,17 +47,152 @@ def deplasare(vector, x):
 
 def eliminare(vector, a, b):
     for i in range(len(vector)):
-        if vector[i]<=a and vector[i]>=b:
+        if vector[i]<a and vector[i]>b:
             print(vector[i])
 
+################################### OPERATII CU MATRICE ###################################
 
-def afisare_vector(vector):
-    print("Vectorul este: ", *vector)
+def citire_matrice(mat):
+    n, m = map(int, input("Introdu numarul de linii si coloane, separate prin spatiu: ").split())
+    for i in range(n):
+        valori = input(f"Introdu {m} elemente pentru linia {i+1}, separate prin spatiu: ")
+        while len(valori.split())!=m:
+            valori = input(f"Introdu {m} elemente pentru linia {i+1}, separate prin spatiu: ")
+        mat.append(list(map(int, valori.split())))
+    return mat
+
+def afisare_matrice(mat):
+    for linie in mat:
+        for elem in linie:
+            print(f"{elem:5d}", end="")
+        print()
+
+def maxx_linie(mat):
+    print("Elementele maxime de pe fiecare linie sunt: ", end="")
+    for linie in mat:
+        el_max = linie[0]
+        for element in linie:
+            if element>el_max: el_max = element
+        print(el_max, end=" ")
+
+def maxx_coloana(mat):
+    print("Elementele maxime de pe fiecare coloana sunt: ", end="")
+    for coloana in zip(*mat):
+        print(coloana)
+        el_max = coloana[0]
+        for element in coloana:
+            if element>el_max: el_max = element
+        print(el_max, end=" ")
+
+def transpusa(mat):
+    n = len(mat)
+    m = len(mat[0])
+    for j in range(m):
+        for i in range(n):
+            print(mat[i][j], end=" ")
+        print()
+
+def adauga_linie(mat):
+    m = len(mat[0])
+    valori = input(f"Introdu {m} elemente pentru linia noua, separate prin spatiu: ")
+    while len(valori.split())!=m:
+        valori = input(f"Introdu {m} elemente pentru linia noua, separate prin spatiu: ")
+    mat.append([int(el) for el in valori.split()])
+    print("Matricea noua este: ")
+    for linie in mat:
+        for elem in linie:
+            print(f"{elem:5d}", end="")
+        print()
+
+
+def adauga_coloana(mat):
+    n = len(mat)
+    valori = input(f"Introdu {n} elemente pentru coloana noua, separate prin spatiu: ").split()
+    while len(valori)!=n:
+        valori = input(f"Introdu {n} elemente pentru coloana noua, separate prin spatiu: ").split()
+    coloana = [int(x) for x in valori]
+    for i in range(n):
+        mat[i].append(coloana[i])
+    print("Matricea noua este: ")
+    for linie in mat:
+        for elem in linie:
+            print(f"{elem:5d}", end="")
+        print()
+
+def sterge_linie(mat):
+    n = int(input("Introdu numarul liniei pe care vrei sa o stergi: "))-1
+    del mat[n]
+    print("Matricea noua este: ")
+    for linie in mat:
+        for elem in linie:
+            print(f"{elem:5d}", end="")
+        print()
+
+def sterge_coloana(mat):
+    n = int(input("Introdu numarul coloanei pe care vrei sa o stergi: "))-1
+    for linie in mat:
+        del linie[n]
+    for linie in mat:
+        for elem in linie:
+            print(f"{elem:5d}", end="")
+        print()
+
+def linializare(mat):
+    n, m = len(mat), len(mat[0])
+    vector = []
+    for i in range(n):
+        for j in range(m):
+            vector.append(str(mat[i][j]))
+    print("Linializare matrice: ", ", ".join(vector))
+            
+
+def matrice():
+    mat = []
+    while True:
+        print("\n" + "━" * 10 + " <[MENIU MATRICE]> " + "━" * 10 + "\n"
+              "1. Citire matrice (pe linii)\n"
+              "2. Afisare matrice\n"
+              "3. Afisare elemente maxime (pe linii)\n"
+              "4. Afisare elemente maxime (pe coloane)\n"
+              "5. Afisare matrice transpusa\n"
+              "6. Adauga linie\n"
+              "7. Adauga coloana\n"
+              "8. Sterge linie\n"
+              "9. Sterge coloana\n"
+              "0. Linializare matrice\n"
+              "X. Revenire meniu principal\n\n"
+              "Optiunea dumneavoastra: ", end="")
+        choice = getch().decode().upper()
+        print(choice)
+        match choice.upper():
+            case "1":
+                mat = citire_matrice(mat)
+            case "2":
+                afisare_matrice(mat)
+            case "3":
+                maxx_linie(mat)
+            case "4":
+                maxx_coloana(mat)
+            case "5":
+                transpusa(mat)
+            case "6":
+                adauga_linie(mat)
+            case "7":
+                adauga_coloana(mat)
+            case "8":
+                sterge_linie(mat)
+            case "9":
+                sterge_coloana(mat)
+            case "0":
+                linializare(mat)
+            case "X":
+                main()
+
 
 def main():
     a, b, n, x = 0, 0, 0, 0
     while True:
-        print("\n"+ "━" * 10 + " <[MENIU]> " + "━" * 10 + "\n"
+        print("\n"+ "━" * 10 + " <[MENIU PRINCIPAL]> " + "━" * 10 + "\n"
               "A. Generare vector de n elemente\n"
               "B. Afisare vector generat\n"
               "C. Afisare elemente > media aritmetica a vectorului\n"
@@ -62,6 +200,7 @@ def main():
               "E. Deplasare elemente cu x pozitii\n"
               "F. Eliminare elemente care nu apartin intervalului [a, b]\n"
               "G. Info autor\n"
+              "M. Meniu matrice\n"
               "H. Exit\n\n"
               "Optiunea dumneavoastra: ", end="")
         choice = getch().decode().upper()
@@ -79,5 +218,12 @@ def main():
                 deplasare(vector, x)
             case "F":
                 eliminare(vector, a, b)
+            case "G":
+                print("Info autor: Tanase Constantin")
             case "H":
-                exit()      
+                exit()
+            case "M":
+                matrice()
+
+if __name__ == '__main__':
+    main()
---
+++ Probleme de antrenament/Diverse/main.py
@@ -0,0 +1,123 @@
+from getch import getch
+
+def suma_n(n):
+    sum = 0
+    n = int(input("Introdu un numar natural n: "))
+    for i in range(1, n+1):
+        sum+=i
+    print(f"Suma primelor [{n}] numere naturale este: {sum}")
+
+def cmmdc(a, b):
+    print(f"Cel mai mare divizor comun dintre [{a}] si [{b}] este: ", end="")
+    while b:
+        a, b = b, a%b
+    print(a)
+
+def inversare(n):
+    n = [int(el) for el in input("Introdu o lista de numere intregi, separate prin spatiu: ").split()]
+    for i in range(len(n)-1, -1, -1):
+        print(n[i], end=" ")
+
+def nr_cuvinte(s):
+    s = input("Introdu un sir de caractere: ")
+    print(f"Sirul contine {len(s.split())} cuvinte!")
+
+def maxx(n):
+    n = [int(el) for el in input("Introdu o lista de numere intregi, separate prin spatiu: ").split()]
+    maxxx = n[0]
+    minn = n[0]
+    for i in n:
+        if i>maxxx:
+            maxxx = i
+        elif i<minn:
+            minn = i
+    print(f"Cel mai mare element din lista este: {maxxx}\nCel mai mic element din lista este: {minn}")
+
+def este_prim(n):
+    while n<=1:
+        n = int(input("Introdu o valoare pentru n, aceasta trebuie sa fie mai mare decat 1: "))
+    prim_sau_nu = True
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            prim_sau_nu = False
+            break
+    if prim_sau_nu: print(f"Numarul [{n}] este prim!")
+    else: print(f"Numarul [{n}] nu este prim!")
+
+def s_palindrom(s):
+    s = input("Introdu un sir de caractere: ")
+    cuvinte = s.split()
+    invers = []
+    for i in range(len(cuvinte)-1, -1, -1):
+        invers.append(cuvinte[i])
+    invers_str = ' '.join(invers)
+    if invers_str == s: print("Sirul introdus este palindrom!")
+    else: print("Sirul introdus nu este palindrom!")
+
+def este_perfect(n):
+    n = int(input("Introdu o valoare pentru n: "))
+    suma_d = 0
+    for i in range(1, n):
+        if n%i==0:
+            suma_d+=i
+    if suma_d == n: print(f"Numarul [{n}] este perfect!")
+    else: print(f"Numarul [{n}] nu este perfect!")
+
+def sortare(n):
+    n = [int(el) for el in input("Introdu o lista de numere intregi, separate prin spatiu: ").split()]
+    ordine = input("Cum vrei sa ordonezi lista? Scrie 'C' pentru a ordona lista crescator, sau 'D' pentru descrescator: ")
+    match ordine.upper():
+        case "C":
+            n.sort()
+            print("Lista sortata crescator este: ", *n)
+        case "D":
+            n.sort(reverse=True)
+            print(f"Lista sortata descrescator este: ", *n)
+
+def main():
+    n = 0
+    s = 0
+    while True:
+        print("\n" + "━" * 10 + " <[MENIU]> " + "━" * 10 + "\n"
+              "1. Calculeaza suma primelor n numere naturale\n"
+              "2. CMMDC a doua numere intregi\n"
+              "3. Inverseaza ordinea unei liste\n"
+              "4. Determina numarul de cuvinte dintr-un sir\n"
+              "5. Determina cel mai mare si cel mai mic element dintr-o lista\n"
+              "6. Determina daca un numar este prim sau nu\n"
+              "7. Determina daca un sir este palindrom\n"
+              "8. Determina daca un numar este perfect sau nu\n"
+              "9. Sorteaza o lista in ordine crescatoare/descrescatoare\n"
+              "0. Iesire\n\n"
+              "Optiunea dumneavoastra: ", end="", flush=True)
+        choice = getch().decode().upper()
+        print(choice)
+        match choice:
+            case "1":
+                suma_n(n)
+            case "2":
+                valori = input("Introdu doua numere intregi, separate prin spatiu: ")
+                a, b = valori.split()
+                a = int(a)
+                b = int(b)
+                cmmdc(a, b)
+            case "3":
+                inversare(n)
+            case "4":
+                nr_cuvinte(s)
+            case "5":
+                maxx(n)
+            case "6":
+                este_prim(n)
+            case "7":
+                s_palindrom(s)
+            case "8":
+                este_perfect(n)
+            case "9":
+                sortare(n)
+            case "0":
+                exit()
+
+
+if __name__ == '__main__':
+    main()
